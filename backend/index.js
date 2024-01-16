6b1fc555d8de1aba3823e68d2768f891cd67052d
Martin <ilhammartin2@gmail.com>
2024-01-16 21:01:43 +0700

    koneksi

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose')
+const express = require('express')
+const { ServerApiVersion } = require('mongodb');
 
 const url = "mongodb+srv://martinilham15:2Azct32sNLJqNgj2@myserver.aw1tteh.mongodb.net/?retryWrites=true&w=majority";
+
+const app = express()
+mongoose.connect(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
