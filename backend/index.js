4981a8a3895a1d7c1c3304b140430e4886fa17cd
Martin <ilhammartin2@gmail.com>
2024-01-16 21:06:35 +0700

    konesi kedatabase mongodb
    berhasil konek

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,20 @@ const mongoose = require('mongoose')
 const express = require('express')
 const { ServerApiVersion } = require('mongodb');
 
-const url = "mongodb+srv://martinilham15:2Azct32sNLJqNgj2@myserver.aw1tteh.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://martinilham15:2Azct32sNLJqNgj2@myserver.aw1tteh.mongodb.net/databarang?retryWrites=true&w=majority";
+const port = 5000
 
 const app = express()
-mongoose.connect(uri, {
+mongoose.connect(url, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
       deprecationErrors: true,
     }
   });
+
+const db = mongoose.connection;
+db.on('error', (error)=>(console.log(error)));
+db.once('open',()=>(console.log("databases connected")))
+
+app.listen(port,()=>console.log(`web berjalan di port ${port}`))
