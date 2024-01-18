382dd0c3a82e6593c1f664051d338093c7bae4f4
Martin <ilhammartin2@gmail.com>
2024-01-18 19:30:29 +0700

    setup frontend

---
+++ frontend/src/components/userList.js
@@ -0,0 +1,65 @@
+import react, { useEffect, useState } from 'react'
+
+
+
+const UserList = ()=>{
+    const [users,setUser] = useState([])
+
+    useEffect(()=>{
+        getUser()
+    },[])
+
+    const getUser = async ()=>{
+        const response = axios.get("http://localhost:5000/users")
+        setUser(response.data)
+    }
+
+    return(
+        <>
+        <div className="columns mt-5">
+      <div className="column is-half">
+        <Link to="add" className="button is-success">
+          Add New
+        </Link>
+        <table className="table is-striped is-fullwidth mt-2">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>
+                  <Link
+                    to={`edit/${user._id}`}
+                    className="button is-info is-small mr-1"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => deleteUser(user._id)}
+                    className="button is-danger is-small"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+        </>
+    )
+}
+
+export default UserList;
